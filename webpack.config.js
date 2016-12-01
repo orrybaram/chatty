44cb13b44417f8b63fc524c79d1843a95d65b184
Orry Baram <orry@mic.com>
2016-12-01 15:34:59 -0500

    adds in webpack and .babelrc

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+const nestedCss = require('postcss-nested');
+const cssCustomProperties = require('postcss-custom-properties');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './client/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('[name].css')
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      }
+    ]
+  },
+  postcss: function() {
+    return [precss, autoprefixer, nestedCss, cssCustomProperties];
+  }
+};
