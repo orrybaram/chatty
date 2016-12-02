f78018f0baa4215eedf7d14f35158261c5577487
Orry Baram <orry@mic.com>
2016-12-02 17:17:05 -0500

    adds server api to store

--- src/shared/stores/MessageStore.js
+++ src/shared/stores/MessageStore.js
@@ -1,8 +1,16 @@
 import {observable, action} from 'mobx';
+import { getChatMessagesById, saveChatMessages } from '../../server/api';
 
 export default class MessageStore {
   @observable messages = [];
   constructor(id) {
     this.id = id;
+  }
+
+  @action loadMessages() {
+    this.messages = getChatMessagesById(this.id);
+  }
+  @action saveMessages() {
+    saveChatMessages({id: this.id, messages: this.messages});
   }
 }
