0cd75cbdb96db1bfe108d32b22f04b6cc911c8f5
Orry Baram <orrybaram@gmail.com>
2016-12-01 23:23:00 -0500

    adds tests for chatStore

---
+++ __tests__/stores/ChatStore.js
@@ -0,0 +1,20 @@
+import ChatStore from '../../src/shared/stores/ChatStore';
+
+describe('ChatStore', () => {
+  const chat = new ChatStore();
+  describe('sendMessage', () => {
+    it('updates the messages array and clears input', () => {
+      chat.inputValue = 'beep boop';
+      chat.sendMessage();
+      expect(chat.messages[0].body).toBe('beep boop');
+      expect(chat.inputValue).toBe('');
+    });
+  });
+  describe('updateInput', () => {
+    it('updates the inputValue', () => {
+      chat.inputValue = '';
+      chat.updateInput({ target: { value: 'beep' } });
+      expect(chat.inputValue).toBe('beep');
+    });
+  });
+});
