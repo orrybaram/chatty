013d350d14f32a1baf911fb61ab6392131c7cde8
Orry Baram <orry@mic.com>
2016-12-02 15:50:34 -0500

    adds tests for server stuff

--- __tests__/server/api.js
+++ __tests__/server/api.js
@@ -1,10 +1,48 @@
-import { getUser, getChat } from '../../src/server/api';
+import { getUser, getChatMessages } from '../../src/server/api';
 
-const localStorage = {};
+const mockChatMessages = [
+  {
+    id: 0,
+    messages: [
+      {
+        body: 'Hi',
+        dateCreated: '12/2/2016',
+        sentBy: {
+          name: 'Bags'
+        }
+      },
+      {
+        body: 'Sup',
+        dateCreated: '12/2/2016',
+        sentBy: {
+          name: 'Gary'
+        }
+      }
+    ]
+  }
+];
+
+beforeAll(() => {
+  localStorage.setItem('ChatMessages', JSON.stringify(mockChatMessages));
+});
 
 describe('getUser', () => {
   it('gets a user by id', () => {
-    expect(getUser(0)).toBe('Rob');
-    expect(getUser(1)).toBe('Rob');
+    expect(getUser(0).name).toBe('Rob');
+    expect(getUser(1).name).toBe('Laura');
+    expect(getUser(2)).toBeUndefined();
+  });
+});
+
+describe('getChatMessages', () => {
+  it('returns chat history by id', () => {
+    const chatHistory = getChatMessages(0);
+    expect(chatHistory.id).toBe(0);
+    expect(chatHistory.messages.length).toBe(2);
+  });
+
+  it('returns undefined if not found', () => {
+    const chatHistory = getChatMessages(1);
+    expect(chatHistory).toBeUndefined();
   });
 });
--- src/server/api.js
+++ src/server/api.js
@@ -1,6 +1,8 @@
 import Users from './data/Users';
 
-const ChatHistory = localStorage.getItem('ChatHistory') || [];
-
 export const getUser = id => Users.filter(({ id: userId }) => id === userId)[0];
-export const getChatHistory = id => ChatHistory.filter(({ id: chatId }) => id === chatId)[0];
+
+export const getChatMessages = (id) => {
+  const ChatMessages = JSON.parse(localStorage.getItem('ChatMessages')) || [];
+  return ChatMessages.filter(({ id: chatId }) => id === chatId)[0];
+};
