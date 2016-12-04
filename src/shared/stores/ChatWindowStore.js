de209229146460602dfdefa25c90b3ad6230739a
Orry Baram <orrybaram@gmail.com>
2016-12-04 18:48:59 -0500

    renames chatstore

---
+++ src/shared/stores/ChatWindowStore.js
@@ -0,0 +1,29 @@
+import { observable, action } from 'mobx';
+
+export default class ChatWindowStore {
+  @observable recipients = [];
+  @observable messages = [];
+  @observable inputValue = '';
+
+  constructor(user, recipients, messageStore) {
+    this.user = user;
+    this.recipients = recipients;
+    this.messageStore = messageStore;
+    this.messages = this.messageStore.messages;
+  }
+
+  @action sendMessage() {
+    const message = {
+      body: this.inputValue,
+      dateCreated: new Date(),
+      sentBy: this.user
+    };
+    this.messages.push(message);
+    this.inputValue = '';
+    this.messageStore.saveMessages();
+  }
+
+  @action updateInput(e) {
+    this.inputValue = e.target.value;
+  }
+}
